189b797f1fb785262f6e64dc5b5263a4342fa44c
Julian Wöhrer <julian.woehrer@gmail.com>
2021-04-05 01:00:23 +0200

    start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@
 
 sleep 30
 python3 ./sondensucher/Python/Database.py
-python3 ./sondensucher/Python/mqtt.py >/dev/null 2>&1
+python3 ./sondensucher/Python/mqtt.py & >/dev/null 2>&1
 python3 ./sondensucher/Python/loop.py
