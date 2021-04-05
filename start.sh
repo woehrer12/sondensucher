85cb1234bac1f98769610d5d90f28e9746e6880c
Julian Wöhrer <julian.woehrer@gmail.com>
2021-04-05 22:59:20 +0200

    start.sh changed

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@
 
 sleep 15
 python3 ./sondensucher/Python/Database.py
-python3 ./sondensucher/Python/mqtt.py & >/dev/null 2>&1
-python3 ./sondensucher/Python/loop.py >/dev/null 2>&1
+python3 ./sondensucher/Python/mqtt.py &
+python3 ./sondensucher/Python/loop.py
