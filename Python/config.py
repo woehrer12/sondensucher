f6821e7527a0a1857430f2c9ec230335892cc9d2
Julian Wöhrer <julian.woehrer@gmail.com>
2021-04-09 18:19:29 +0200

    Config Version Check

--- Python/config.py
+++ Python/config.py
@@ -3,32 +3,50 @@ import logging
 import configparser
 import os
 
+config_version = '1.0'
+
+
+def create(config):
+    logging.info("Config File angelegt")
+    config['DEFAULT'] = {
+        'dbpfad': 'db',
+        'dbuser': 'sondensucher',
+        'dbpassword': 'sondensucher',
+        'dbname': 'sonden',
+        # Deaktivierbare Teile
+        'getradiosondycsv': '1',
+        'getsondehub': '1',
+        'gethoehen': '1',
+        'mqtt-sondensucher.de': '1',
+        'API': '1',
+        'config_version': config_version,
+    }
+
+    with open('config/config.ini', 'w') as configfile:
+        config.write(configfile)
+
+
 def config():
     try:
+
         # Config Datei anlegen und auslesen
         config = configparser.ConfigParser()
         if os.path.isfile("config/config.ini"):
             ("Config File gefunden")
+            config.read('config/config.ini')
+            conf = config['DEFAULT']
+            if conf['config_version'] != config_version:
+                os.remove("config/config.ini")
+                logging.info("Config gelöscht")
+                create(config)
+            else:
+                logging.info("Config Version ok")
         else:
-            print("Config File angelegt")
-            config['DEFAULT'] = {
-                            'dbpfad': 'db',
-                            'dbuser': 'sondensucher',
-                            'dbpassword': 'sondensucher',
-                            'dbname': 'sonden',
-                            #Deaktivierbare Teile
-                            'getradiosondycsv': '1',
-                            'getsondehub': '1',
-                            'gethoehen': '1',
-                            'mqtt-sondensucher.de': '1',
-                            'API': '1',
-                            }
-
-            with open('config/config.ini', 'w') as configfile:
-                config.write(configfile)
+            create(config)
 
         config.read('config/config.ini')
         conf = config['DEFAULT']
     except:
         print("Unexpected error Config anlegen Database.py:" + str(sys.exc_info()))
-        logging.error("Unexpected error Config anlegen Database.py:" + str(sys.exc_info()))
+        logging.error(
+            "Unexpected error Config anlegen Database.py:" + str(sys.exc_info()))
--- Python/getsondehub.py
+++ Python/getsondehub.py
@@ -7,7 +7,7 @@ import json
 import datetime
 
 def csv(mydb):
-    try:
+    #try:
         mycursor = mydb.cursor()
         response = requests.get("https://api.v2.sondehub.org/sondes/telemetry",  timeout=30)
         logging.info(response.url)
@@ -15,47 +15,48 @@ def csv(mydb):
         if response.status_code == 200:
             logging.info('Success!')
             r = response.json()
-            
+
             for i in r.keys():
                 print(i)
                 logging.info(i)
                 s = response.json()[i]
                 for j in s.keys():
+                    dateneintrag(mydb,response,i,j)
                     
-                    
-                    sondenid = j
-                    lat = response.json()[i][j]['lat']
-                    lon = response.json()[i][j]['lon']
-                    alt = response.json()[i][j]['alt']
-                    try:
-                        geschw = response.json()[i][j]['vel_h']
-                    except:
-                        geschw = 0.0
-                    try:
-                        vgeschw = response.json()[i][j]['vel_v']
-                    except:
-                        vgeschw = 0.0                    
-                    try:
-                        richtung = response.json()[i][j]['heading']
-                    except:
-                        richtung = 0.0
-                    try:
-                        freq = response.json()[i][j]['frequency']
-                    except:
-                        freq = 0.0
-                    sondetime = int(datetime.datetime.strptime(response.json()[i][j]['datetime'],'%Y-%m-%dT%H:%M:%S.%f%z').timestamp())
-
-                    payload="INSERT INTO sonden (sondenid, lat, lon, hoehe, geschw, vgeschw, richtung, freq, sondetime, server) VALUES ('" + i + "', " + str(lat) + ", " + str(lon) + ", " + str(alt) + ", " + str(geschw) + ", " + str(vgeschw) + ", " + str(richtung) + ", " + str(freq) + ", " + str(sondetime) + ", 'Sondehub')"
-                    #print(payload)
-                    mycursor.execute(payload)        
-                    mydb.commit()
-
-
+                
             
 
         elif response.status_code == 404:
             print('Not Found.')
+    #except:
+    #    print("Unexpected error getsondehub.py:" + str(sys.exc_info()))
+    #    logging.error("Unexpected error getsondehub.py:" + str(sys.exc_info()))
+    #   return None
+
+
+def dateneintrag(mydb,response,i,j):
+    sondenid = j
+    lat = response.json()[i][j]['lat']
+    lon = response.json()[i][j]['lon']
+    alt = response.json()[i][j]['alt']
+    try:
+        geschw = response.json()[i][j]['vel_h']
     except:
-        print("Unexpected error getsondehub.py:" + str(sys.exc_info()))
-        logging.error("Unexpected error getsondehub.py:" + str(sys.exc_info()))
-        return None
+        geschw = 0.0
+    try:
+        vgeschw = response.json()[i][j]['vel_v']
+    except:
+        vgeschw = 0.0                    
+    try:
+        richtung = response.json()[i][j]['heading']
+    except:
+        richtung = 0.0
+    try:
+        freq = response.json()[i][j]['frequency']
+    except:
+        freq = 0.0
+    sondetime = int(datetime.datetime.strptime(response.json()[i][j]['datetime'],'%Y-%m-%dT%H:%M:%S.%f%z').timestamp())
+    mycursor = mydb.cursor()
+    payload="INSERT INTO sonden (sondenid, lat, lon, hoehe, geschw, vgeschw, richtung, freq, sondetime, server) VALUES ('" + i + "', " + str(lat) + ", " + str(lon) + ", " + str(alt) + ", " + str(geschw) + ", " + str(vgeschw) + ", " + str(richtung) + ", " + str(freq) + ", " + str(sondetime) + ", 'Sondehub')"
+    mycursor.execute(payload)        
+    mydb.commit()
