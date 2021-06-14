5bce36ac9af270e693c087bd602947ff3df2c61a
Julian Wöhrer <julian.woehrer@gmail.com>
2021-06-14 23:28:54 +0200

    loop in start eingefügt

--- start.sh
+++ start.sh
@@ -8,8 +8,7 @@ python3 ./sondensucher/Python/config.py
 python3 ./sondensucher/Python/Database.py
 sleep 15
 
-
-
 python3 ./sondensucher/Python/mqtt.py &
 python3 ./sondensucher/Python/API.py &
+python3 ./sondensucher/Python/loop.py
 sleep 600000
